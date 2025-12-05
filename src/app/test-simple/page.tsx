890c3fa8bc332bd2f67efd03e3058beaf065d1bb
Nayzex <nathan.swk.71@gmail.com>
2025-12-06 00:43:22 +0100

    clean de tout ce qui est inutile

--- src/app/test-simple/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SimpleTestPage() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Portfolio Nayzex - Test Simple</h1>
-      <p>Cette page teste que Next.js fonctionne sans next-intl.</p>
-      <p>Si vous voyez cette page, l&apos;application fonctionne !</p>
-    </div>
-  );
-}
